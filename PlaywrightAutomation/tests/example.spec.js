f9d4e5c3e530ae0623dbb250e3fa84605e435eda
srivass2 <shashank.srivastava2@mail.dcu.ie>
2025-09-07 17:13:23 +0100

    first commit

---
+++ PlaywrightAutomation/tests/example.spec.js
@@ -0,0 +1,31 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test.only('My First Test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+  await expect(page).toHaveTitle('OrangeHRM')
+  await page.getByRole('textbox', { name: 'Username'}).fill('Admin')
+  await page.getByRole('textbox', { name: 'Password'}).fill('admin123')
+  await page.getByRole('button', { name: 'Login'}).click()
+
+  await page.pause()
+
+});
